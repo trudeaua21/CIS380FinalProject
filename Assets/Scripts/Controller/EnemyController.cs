b4fe126b942dcbdd0192a592990879dee37e3fae
Dalton Claybaugh <claybaud@mail.gvsu.edu>
2021-04-25 22:30:45 -0400

    Animation Work

--- Assets/Scripts/Controller/EnemyController.cs
+++ Assets/Scripts/Controller/EnemyController.cs
@@ -11,12 +11,15 @@ public class EnemyController : MonoBehaviour
     private const float BASE_SPEED = 3;
 
     private float iFrames;
-
     private float CurrentSpeed;
-
     private float lastTime;
 
-    private bool isStunned;
+    private bool isAttacking;
+    private bool isWalking;
+    private bool isDying;
+    private bool isResting;
+    private bool isTakingDamage;
+    private bool isDead;
 
     private Vector3 lastPosition;
     private Animator animator;
@@ -42,35 +45,39 @@ public class EnemyController : MonoBehaviour
         lastPosition = transform.position;
         lastTime = Time.deltaTime;
         iFrames = 3f;
+        isDead = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        AnimatorUpdater();
         iFrames -= Time.deltaTime;
-        if(stats.currentHealth > 0){
-            SetCurrentSpeed(lastPosition, transform.position);
-            animator.SetFloat("speed", CurrentSpeed);
-            float distance = Vector3.Distance(target.position, transform.position);
+        SetCurrentSpeed(lastPosition, transform.position);
+        animator.SetFloat("speed", CurrentSpeed);
 
-            if(distance <= visionRadius)
-            {
-                agent.SetDestination(target.position);
-                UnityEngine.AI.NavMeshHit hit;
-                if(!agent.Raycast(target.position, out hit)){
-                    if(distance <= agent.stoppingDistance)
-                    {
-                        Attack_1();
-                        FaceTarget();
-                    } 
-                    else if(combat.attackCooldown <= 2)
-                    {
-                        agent.speed = BASE_SPEED;
+        if((!isTakingDamage || !isAttacking || !isDying) && !isDead){
+                
+                float distance = Vector3.Distance(target.position, transform.position);
+
+                if(distance <= visionRadius)
+                {
+                    agent.SetDestination(target.position);
+                    UnityEngine.AI.NavMeshHit hit;
+                    if(!agent.Raycast(target.position, out hit)){
+                        if(distance <= agent.stoppingDistance)
+                        {
+                            Attack_1();
+                            FaceTarget();
+                        } 
+                        else if(combat.attackCooldown <= 2)
+                        {
+                            agent.speed = BASE_SPEED;
+                        }
                     }
                 }
-            }
+            
         }
-
     }
 
     private void OnTriggerEnter(Collider other){
@@ -83,7 +90,6 @@ public class EnemyController : MonoBehaviour
                 if(stats.currentHealth > 0){
                     animator.Play("Base Layer.Armature|TakeDamage", 0, .25f);
                 }
-                
             }
             agent.speed = 0f;
         }
@@ -111,6 +117,44 @@ public class EnemyController : MonoBehaviour
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
 
+    private void AnimatorUpdater(){
+        if(animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Armature|Attack_1")){
+            isAttacking = true;
+        } else {
+            isAttacking = false;
+        }
+
+        if(animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Armature|TakeDamage")){
+            isTakingDamage = true;
+        } else {
+            isTakingDamage = false;
+        }
+
+        if(animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Armature|Walk_Cycle_1")){
+            isWalking = true;
+        } else {
+            isWalking = false;
+        }
+
+        if(animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Armature|Die")){
+            isDying = true;
+        } else {
+            isDying = false;
+        }
+
+        if(animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Armature|Die")){
+            isDying = true;
+        } else {
+            isDying = false;
+        }
+
+        if(animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Armature|Rest_1")){
+            isResting = true;
+        } else {
+            isResting = false;
+        }
+
+    }
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
