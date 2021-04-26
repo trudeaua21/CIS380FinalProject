07dd80d152bfcc516aa521e4da6bfd13a37c532e
Cameron Shearer <47761419+Sheareca@users.noreply.github.com>
2021-04-25 22:34:28 -0400

    Health Bar
    Nice Bar

--- Assets/Scripts/Stats/CharacterStats.cs
+++ Assets/Scripts/Stats/CharacterStats.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 /* Base class that player and enemies can derive from to include stats. */
 // Source Code based on https://github.com/Brackeys/RPG-Tutorial
 
@@ -12,7 +12,7 @@ public class CharacterStats : MonoBehaviour
 
 	public Stat damage;
 	public Stat armor;
-
+	public Text healthBar;
 	Animator animator;
 
 	// Set current health to max health
@@ -41,8 +41,13 @@ public class CharacterStats : MonoBehaviour
 			Die();
 		}
 	}
-
-	public virtual void Die()
+	void Update()
+	{
+		if (healthBar != null) { 
+		healthBar.text = "Health= " + currentHealth + "/" + maxHealth;
+		}
+    }
+    public virtual void Die()
 	{
 		// Die in some way
 		// This method is meant to be overwritten
